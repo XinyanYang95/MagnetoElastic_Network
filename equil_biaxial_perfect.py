7c88b7cb908f9708ead0d6ff52f79df72ecb1e62
Xinyan Yang <55174618+XinyanYang95@users.noreply.github.com>
2023-07-13 09:11:25 -0500

    Update equil_biaxial_perfect.py
    revised F mag

--- equil_biaxial_perfect.py
+++ equil_biaxial_perfect.py
@@ -497,7 +497,7 @@ def Force_mag_steric_repulsion_map(pair, magr, pos, eps=3.0e-2):
     # LJ 50-49
     # zero-energy position
     sig = cridis*49/50 # in cm
-    F_rep=jnp.where(distemp < cridis, 50*(50/49)**49*eps*(-sig**50/distemp**51+sig**49/distemp**50)*100*n12, 0*n12)
+    F_rep=jnp.where(distemp < cridis, 50*(50/49)**49*eps*(-50*sig**50/distemp**51+49*sig**49/distemp**50)*100*n12, 0*n12)
     
     tempspace = jnp.zeros([magnum,3])
     tempspace = tempspace.at[pair[0]].set(-F_rep)
